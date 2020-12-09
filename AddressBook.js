edaaddbf82bac5f61f44935215efc6ca244f281b
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-09 21:13:25 +0530

    [Prudhvi] add . Creating contact

---
+++ AddressBook.js
@@ -0,0 +1,14 @@
+//UC-1
+class Contact {
+    // Constructor for the different fields of contact
+    constructor(...params) {
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.email = params[7];
+    }
+}
