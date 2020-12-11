d922f032bde4afbf3da24949659afba648dc5f62
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-11 07:15:40 +0530

    [Prudhvi] refractor get and set methods

--- AddressBook.js
+++ AddressBook.js
@@ -11,4 +11,42 @@ class Contact {
         this.phoneNumber = params[6];
         this.email = params[7];
     }
+// Getter & Setter method
+get firstName() { return this._firstName; }
+set firstName(firstName) {
+    this._firstName = firstName;
+}
+get lastName() { return this._lastName; }
+set lastName(lastName) {
+    this._lastName = lastName;
+}
+get address() { return this._address; }
+set address(address) {
+    this._address = address;
+}
+get city() { return this._city; }
+set city(city) {
+    this._city = city;
+}
+get state() { return this._state; }
+set state(state) {
+    this._state = state;
+}
+get zip() { return this._zip; }
+set zip(zip) {
+    this._zip = this.zip;
+}
+get phoneNumber() { return this._phoneNumber; }
+set phoneNumber(phoneNumber) {
+    this._phoneNumber = phoneNumber;
+}
+get email() { return this._email; }
+set email(email) {
+    this._email = email;
+}
+//To string method for displaying contacts
+toString() {
+    return "First Name: " + this.firstName + " Last Name: " + this.lastName + " Address: " + this.address + " City: " + this.city
+        + " State: " + this.state + " Zipcode: " + this.zip + " Phone Number: " + this.phoneNumber + " Email: " + this.email;
+}
 }
